c007e6b3aaeb5a93cc3b671146426ad5bb4ad2d2
Eden Sun <huntack478@gmail.com>
2015-08-27 23:37:04 +0800

    整理code 加 保留read 相容

--- ARDUINO-24FC512.ino
+++ ARDUINO-24FC512.ino
@@ -16,44 +16,31 @@ void setup(void) {
 }
 
 void loop(void) {
-    byte number = Serial.readBytes(buffer, 105);
-    unsigned char mode = buffer[0];
+  byte number = Serial.readBytes(buffer, 105);
+  unsigned char mode = buffer[0];
+  if(number == 105 && mode==WRITE){
+    
     unsigned int address = (((unsigned int)buffer[1])<<8) + (unsigned int)buffer[2];
     unsigned char length = buffer[3];
     byte check = 0;
     for(unsigned char i = 0;i<104;i++){
-        check+=buffer[i];
+      check+=buffer[i];
     }
-    if(mode==WRITE){
-        if (check!=buffer[104]) // fail 
-        {
-            Serial.write(FAIL);
-            return;
-        }else {
-            WireEepromWriteEx(theDeviceAddress,address,length,buffer);
-            Serial.write(SUCCESS);
-            return;
-        }
-         
-        
-    }
-    return;
-    if(number==4){
-      if(buffer[0]==0x55){  // write
-          unsigned int address = ((unsigned int)buffer[1])*256;
-          address += (unsigned int)buffer[2];
-          WireEepromWriteByte(theDeviceAddress, address, buffer[3]);
-          Serial.write(buffer[1]^buffer[2]^buffer[3]);  
-        }else if(buffer[0]==0x88){  //read
-          unsigned int address = ((unsigned int)buffer[1])*256;
-          address += (unsigned int)buffer[2];
-          byte dataRead = WireEepromReadByte(theDeviceAddress, address);
-//          Serial.write(buffer[1]);
-//          Serial.write(buffer[2]);
-          Serial.write(dataRead);
-        }
-       
-    }
+    if (check!=buffer[104]){ // fail
+      Serial.write(FAIL);
+      return;
+    }else {
+      WireEepromWriteEx(theDeviceAddress,address,length,buffer);
+      Serial.write(SUCCESS);
+      return;
+    }   
+  }else
+  if(number==4 && buffer[0]== READ ){
+    unsigned int address = ((unsigned int)buffer[1])*256;
+    address += (unsigned int)buffer[2];
+    byte dataRead = WireEepromReadByte(theDeviceAddress, address);
+    Serial.write(dataRead);
+  }else Serial.write(FAIL);
 }
 
 
@@ -75,11 +62,6 @@ byte WireEepromReadByte(int theDeviceAddress, unsigned int theMemoryAddress) {
   return (byte)(((theByteArray[0] << 0)));
 }
 
-int WireEepromReadInt(int theDeviceAddress, unsigned int theMemoryAddress) {
-  byte theByteArray[sizeof(int)];
-  WireEepromRead(theDeviceAddress, theMemoryAddress, sizeof(int), theByteArray);
-  return (int)(((theByteArray[0] << 8)) | (int)((theByteArray[1] << 0)));
-}
 
 void WireEepromWrite(int theDeviceAddress, unsigned int theMemoryAddress, int theByteCount, byte* theByteArray) {
   for (int theByteIndex = 0; theByteIndex < theByteCount; theByteIndex++) {
@@ -103,12 +85,3 @@ void WireEepromWriteEx(int theDeviceAddress, unsigned int theMemoryAddress, unsi
    delay(5);
 }
 
-void WireEepromWriteByte(int theDeviceAddress, unsigned int theMemoryAddress, byte theByte) {
-  byte theByteArray[sizeof(byte)] = {(byte)(theByte >> 0)};
-  WireEepromWrite(theDeviceAddress, theMemoryAddress, sizeof(byte), theByteArray);
-}
-
-void WireEepromWriteInt(int theDeviceAddress, unsigned int theMemoryAddress, int theInt) {
-  byte theByteArray[sizeof(int)] = {(byte)(theInt >> 8), (byte)(theInt >> 0)};
-  WireEepromWrite(theDeviceAddress, theMemoryAddress, sizeof(int), theByteArray);
-}
